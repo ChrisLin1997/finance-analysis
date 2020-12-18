55440749e235aef9115099be827324f3f25cf2b3
ChrisLin <chrisl@cqcp.com.tw>
2020-12-18 20:35:55 +0800

    組件/aside-menu & header-bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<template lang="pug">
+#app
+  header-bar
+  aside-menu
+  //- router-link(to="/") Home
+  //- router-link(to="/about") About
+router-view
 </template>
 
+<script>
+import AsideMenu from '@/components/aside-menu'
+import HeaderBar from '@/components/header-bar'
+
+export default {
+  name: 'app',
+
+  components: {
+    AsideMenu,
+    HeaderBar,
+  },
+}
+</script>
+
 <style lang="scss">
+@import '@/assets/index.scss';
 #app {
+  width: 100vw;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: $background;
 }
 </style>
---
+++ src/components/header-bar/index.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+.header-bar
+  .title
+    span Finance
+    span Analysis
+
+  .type(v-for="type of financeTypes" :key="type.name")
+    .type-item {{ type.label }}
+
+</template>
+
+<script>
+export default {
+  name: 'header-bar',
+
+  setup () {
+    const financeTypes = [
+      { name: 'stock', label: '股票' },
+      { name: 'currency', label: '貨幣' },
+      { name: 'virtualCurrency', label: '虛擬貨幣' },
+    ]
+
+    return {
+      financeTypes,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header-bar {
+  display: flex;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 76px;
+  line-height: 76px;
+  background-color: $primary-background;
+  box-shadow: 0 0 8px 0 #333;
+  z-index: 2;
+}
+
+.title {
+  width: 284px;
+  height: 100%;
+  font-size: 24px;
+  background-color: $background;
+  & > span:first-child {
+    display: inline;
+    color: $active;
+  }
+}
+
+.type {
+  max-width: calc(100% - 284px);
+  height: 100%;
+  overflow: auto;
+}
+
+.type-item {
+  margin: 8px;
+  width: 180px;
+  height: 60px;
+  line-height: 60px;
+  background-color: $active-background;
+  border: 1px solid #666;
+  border-radius: 8px;
+}
+
+</style>
