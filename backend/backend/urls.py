7f0b569bc504af26acb6c40a6ff10fc7a014e122
ChrisLin <i100308326@gmail.com>
2020-12-20 11:25:43 +0800

    backend add cors

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from stock.views import chart
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('stock/chart', chart),
 ]
--- backend/stock/views.py
+++ backend/stock/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def chart (request):
+    print(request.method)
+    return JsonResponse({'status': 200, 'msg': 'stock'})
