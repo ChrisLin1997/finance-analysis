75301dda48cdd2bce4fd135ff59d9120d489b5c0
ChrisLin <i100308326@gmail.com>
2020-12-22 23:50:42 +0800

    backend realtime api

--- backend/stock/views.py
+++ backend/stock/views.py
@@ -7,21 +7,10 @@ import twstock
 cors = 'https://cors-anywhere.herokuapp.com/'
 
 def info (request):
-    stockInfo = twstock.Stock(request.GET['stockId'])
-    dateList = [ datetime.strftime(date, '%Y/%m/%d') for date in stockInfo.date ]
-
-    result = json.dumps({
-        'status': 200,
-        'data': {
-            'date': dateList,
-            'transAmount': stockInfo.turnover,
-            'transactions': stockInfo.transaction,
-            'open': stockInfo.close,
-            'close': stockInfo.price,
-            'high': stockInfo.high,
-            'low': stockInfo.low,
-        }
-    })
+    params = {
+        'stockNo': request.GET('stockNo')
+    }
+    result = requests.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', params = params)
 
     return HttpResponse(result)
 
@@ -29,4 +18,24 @@ def hot (request):
     result = requests.get('https://www.twse.com.tw/exchangeReport/MI_INDEX20')
 
     return HttpResponse(result)
+
+# def realtime (request):
+#     stockInfo = twstock.Stock(request.GET['stockId'])
+#     dateList = [ datetime.strftime(date, '%Y/%m/%d') for date in stockInfo.date ]
+
+#     result = json.dumps({
+#         'status': 200,
+#         'data': {
+#             'date': dateList,
+#             'transAmount': stockInfo.turnover,
+#             'transactions': stockInfo.transaction,
+#             'open': stockInfo.close,
+#             'close': stockInfo.price,
+#             'high': stockInfo.high,
+#             'low': stockInfo.low,
+#         }
+#     })
+
+#     return HttpResponse(result)
+
 
