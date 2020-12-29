b0b49899c663635a3245ee1c2a33e335ebedf546
ChrisLin1997 <i100308326@gmail.com>
2020-12-29 18:07:36 +0800

    loading constructor

--- frontend/src/components/loading/index.vue
+++ frontend/src/components/loading/index.vue
@@ -29,6 +29,7 @@ export default {
 <style lang="scss" scoped>
 .loading {
   display: flex;
+  justify-content: center;
   align-items: center;
 }
 
--- frontend/src/components/loading/loading.js
+++ frontend/src/components/loading/loading.js
@@ -1,3 +1,15 @@
-export default function loading (el, binding) {
-  console.log(el.getBoundingClientRect())
+import { defineComponent} from 'vue'
+const loadingConstructor = defineComponent(require("./index.vue").default)
+
+export default {
+  mounted (el, binding) {
+    // const app = document.getElementById('app')
+    console.log(loadingConstructor.mount())
+    // app.appendChild()
+    const elementInfo = el.getBoundingClientRect()
+    const x = elementInfo.x
+    const y = elementInfo.y
+    const width = elementInfo.width
+    const height = elementInfo.height
+  }
 }
