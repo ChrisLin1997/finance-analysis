b8546c32990e69bffb89ad593b1c262973acb055
ChrisLin <i100308326@gmail.com>
2021-01-10 12:58:15 +0800

    chron: allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2*!0-_0#&++l$_4v4pc9120j=$e3z0o0wc3ljp7(^rc^7mg%#q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # SESSION_COOKIE_SECURE = True
 # SESSION_COOKIE_HTTPONLY = True
@@ -58,7 +58,6 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
-    'https://localhost:8080',
     'http://chrislin1997.github.io',
     'https://chrislin1997.github.io',
 ]
