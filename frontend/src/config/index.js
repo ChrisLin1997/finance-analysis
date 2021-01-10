7e0e8a2977c3bf1a66296b5abe1296614d278cad
ChrisLin <i100308326@gmail.com>
2021-01-10 22:27:23 +0800

    chron:server url fix

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,2 +1,2 @@
 export const CORS = 'https://cors-anywhere.herokuapp.com/'
-export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000/' : CORS + 'https://35.194.188.246:8000/'
+export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000/' : CORS + 'http://35.194.188.246:8000/'
